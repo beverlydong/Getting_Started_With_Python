9bc6e005887076deba18c3b9eef83626ffdac21b
beverlydong <beverly.dong7@gmail.com>
2022-10-06 18:48:31 -0700

    hth_fallprogram

---
+++ main.py
@@ -0,0 +1,13 @@
+full_name = "Beverly Dong"
+nickname = "Bev"
+age = 16
+has_used_python = True
+
+hobbies = ["listening to music", "watching anime", "spending time with my family"]
+fav_things = {
+  "movie" : "Your Name",
+  "food" : "sushi",
+  "hobby" : hobbies[0],
+  "anime show" : "Death Note"
+}
+all_vars = dict(vars())
